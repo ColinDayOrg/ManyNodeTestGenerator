323a59e7ea8ac58a987a968d2896b989793dc409
ColinDayOrg <colin.day@autodesk.com>
2018-12-11 13:50:51 -0500

    First step towards creating a zero touch node file

---
+++ Generator/ManyNodeTestGenerator/FileGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace ManyNodeTestGenerator
+{
+    class FileGenerator
+    {
+        internal static void GenerateFile(
+            string filename,
+            string saveLocation)
+        {
+            string inputText = File.ReadAllText(filename);
+            File.WriteAllText(saveLocation, inputText);
+        }
+    }
+}
