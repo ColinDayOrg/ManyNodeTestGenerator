a5b79e8fff5a1fc3b1ab234f6b6938a6da20cca2
ColinDayOrg <colin.day@autodesk.com>
2018-12-12 08:31:59 -0500

    Add 10 nodes test project

---
+++ ZTNodes/ZTNodes_0010/ZTNodes_0010/ZTNodes_0010.cs
@@ -0,0 +1,53 @@
+namespace ZTNodes
+{
+    public class NodeType_0000 
+    { 
+        private NodeType_0000() {}
+        static public NodeType_0000 Create() => new NodeType_0000(); 
+    }
+    public class NodeType_0001 
+    { 
+        private NodeType_0001() {}
+        static public NodeType_0001 Create() => new NodeType_0001(); 
+    }
+    public class NodeType_0002 
+    { 
+        private NodeType_0002() {}
+        static public NodeType_0002 Create() => new NodeType_0002(); 
+    }
+    public class NodeType_0003 
+    { 
+        private NodeType_0003() {}
+        static public NodeType_0003 Create() => new NodeType_0003(); 
+    }
+    public class NodeType_0004 
+    { 
+        private NodeType_0004() {}
+        static public NodeType_0004 Create() => new NodeType_0004(); 
+    }
+    public class NodeType_0005 
+    { 
+        private NodeType_0005() {}
+        static public NodeType_0005 Create() => new NodeType_0005(); 
+    }
+    public class NodeType_0006 
+    { 
+        private NodeType_0006() {}
+        static public NodeType_0006 Create() => new NodeType_0006(); 
+    }
+    public class NodeType_0007 
+    { 
+        private NodeType_0007() {}
+        static public NodeType_0007 Create() => new NodeType_0007(); 
+    }
+    public class NodeType_0008 
+    { 
+        private NodeType_0008() {}
+        static public NodeType_0008 Create() => new NodeType_0008(); 
+    }
+    public class NodeType_0009 
+    { 
+        private NodeType_0009() {}
+        static public NodeType_0009 Create() => new NodeType_0009(); 
+    }
+}
