2023e3b327bcbf1da30f6f0bfed025de97e8e76e
ColinDayOrg <colin.day@autodesk.com>
2018-12-12 08:35:12 -0500

    Add 100 nodes test project

---
+++ ZTNodes/ZTNodes_0100/ZTNodes_0100/ZTNodes_0100.cs
@@ -0,0 +1,503 @@
+namespace ZTNodes
+{
+    public class NodeType_0000 
+    { 
+        private NodeType_0000() {}
+        static public NodeType_0000 Create() => new NodeType_0000(); 
+    }
+    public class NodeType_0001 
+    { 
+        private NodeType_0001() {}
+        static public NodeType_0001 Create() => new NodeType_0001(); 
+    }
+    public class NodeType_0002 
+    { 
+        private NodeType_0002() {}
+        static public NodeType_0002 Create() => new NodeType_0002(); 
+    }
+    public class NodeType_0003 
+    { 
+        private NodeType_0003() {}
+        static public NodeType_0003 Create() => new NodeType_0003(); 
+    }
+    public class NodeType_0004 
+    { 
+        private NodeType_0004() {}
+        static public NodeType_0004 Create() => new NodeType_0004(); 
+    }
+    public class NodeType_0005 
+    { 
+        private NodeType_0005() {}
+        static public NodeType_0005 Create() => new NodeType_0005(); 
+    }
+    public class NodeType_0006 
+    { 
+        private NodeType_0006() {}
+        static public NodeType_0006 Create() => new NodeType_0006(); 
+    }
+    public class NodeType_0007 
+    { 
+        private NodeType_0007() {}
+        static public NodeType_0007 Create() => new NodeType_0007(); 
+    }
+    public class NodeType_0008 
+    { 
+        private NodeType_0008() {}
+        static public NodeType_0008 Create() => new NodeType_0008(); 
+    }
+    public class NodeType_0009 
+    { 
+        private NodeType_0009() {}
+        static public NodeType_0009 Create() => new NodeType_0009(); 
+    }
+    public class NodeType_0010 
+    { 
+        private NodeType_0010() {}
+        static public NodeType_0010 Create() => new NodeType_0010(); 
+    }
+    public class NodeType_0011 
+    { 
+        private NodeType_0011() {}
+        static public NodeType_0011 Create() => new NodeType_0011(); 
+    }
+    public class NodeType_0012 
+    { 
+        private NodeType_0012() {}
+        static public NodeType_0012 Create() => new NodeType_0012(); 
+    }
+    public class NodeType_0013 
+    { 
+        private NodeType_0013() {}
+        static public NodeType_0013 Create() => new NodeType_0013(); 
+    }
+    public class NodeType_0014 
+    { 
+        private NodeType_0014() {}
+        static public NodeType_0014 Create() => new NodeType_0014(); 
+    }
+    public class NodeType_0015 
+    { 
+        private NodeType_0015() {}
+        static public NodeType_0015 Create() => new NodeType_0015(); 
+    }
+    public class NodeType_0016 
+    { 
+        private NodeType_0016() {}
+        static public NodeType_0016 Create() => new NodeType_0016(); 
+    }
+    public class NodeType_0017 
+    { 
+        private NodeType_0017() {}
+        static public NodeType_0017 Create() => new NodeType_0017(); 
+    }
+    public class NodeType_0018 
+    { 
+        private NodeType_0018() {}
+        static public NodeType_0018 Create() => new NodeType_0018(); 
+    }
+    public class NodeType_0019 
+    { 
+        private NodeType_0019() {}
+        static public NodeType_0019 Create() => new NodeType_0019(); 
+    }
+    public class NodeType_0020 
+    { 
+        private NodeType_0020() {}
+        static public NodeType_0020 Create() => new NodeType_0020(); 
+    }
+    public class NodeType_0021 
+    { 
+        private NodeType_0021() {}
+        static public NodeType_0021 Create() => new NodeType_0021(); 
+    }
+    public class NodeType_0022 
+    { 
+        private NodeType_0022() {}
+        static public NodeType_0022 Create() => new NodeType_0022(); 
+    }
+    public class NodeType_0023 
+    { 
+        private NodeType_0023() {}
+        static public NodeType_0023 Create() => new NodeType_0023(); 
+    }
+    public class NodeType_0024 
+    { 
+        private NodeType_0024() {}
+        static public NodeType_0024 Create() => new NodeType_0024(); 
+    }
+    public class NodeType_0025 
+    { 
+        private NodeType_0025() {}
+        static public NodeType_0025 Create() => new NodeType_0025(); 
+    }
+    public class NodeType_0026 
+    { 
+        private NodeType_0026() {}
+        static public NodeType_0026 Create() => new NodeType_0026(); 
+    }
+    public class NodeType_0027 
+    { 
+        private NodeType_0027() {}
+        static public NodeType_0027 Create() => new NodeType_0027(); 
+    }
+    public class NodeType_0028 
+    { 
+        private NodeType_0028() {}
+        static public NodeType_0028 Create() => new NodeType_0028(); 
+    }
+    public class NodeType_0029 
+    { 
+        private NodeType_0029() {}
+        static public NodeType_0029 Create() => new NodeType_0029(); 
+    }
+    public class NodeType_0030 
+    { 
+        private NodeType_0030() {}
+        static public NodeType_0030 Create() => new NodeType_0030(); 
+    }
+    public class NodeType_0031 
+    { 
+        private NodeType_0031() {}
+        static public NodeType_0031 Create() => new NodeType_0031(); 
+    }
+    public class NodeType_0032 
+    { 
+        private NodeType_0032() {}
+        static public NodeType_0032 Create() => new NodeType_0032(); 
+    }
+    public class NodeType_0033 
+    { 
+        private NodeType_0033() {}
+        static public NodeType_0033 Create() => new NodeType_0033(); 
+    }
+    public class NodeType_0034 
+    { 
+        private NodeType_0034() {}
+        static public NodeType_0034 Create() => new NodeType_0034(); 
+    }
+    public class NodeType_0035 
+    { 
+        private NodeType_0035() {}
+        static public NodeType_0035 Create() => new NodeType_0035(); 
+    }
+    public class NodeType_0036 
+    { 
+        private NodeType_0036() {}
+        static public NodeType_0036 Create() => new NodeType_0036(); 
+    }
+    public class NodeType_0037 
+    { 
+        private NodeType_0037() {}
+        static public NodeType_0037 Create() => new NodeType_0037(); 
+    }
+    public class NodeType_0038 
+    { 
+        private NodeType_0038() {}
+        static public NodeType_0038 Create() => new NodeType_0038(); 
+    }
+    public class NodeType_0039 
+    { 
+        private NodeType_0039() {}
+        static public NodeType_0039 Create() => new NodeType_0039(); 
+    }
+    public class NodeType_0040 
+    { 
+        private NodeType_0040() {}
+        static public NodeType_0040 Create() => new NodeType_0040(); 
+    }
+    public class NodeType_0041 
+    { 
+        private NodeType_0041() {}
+        static public NodeType_0041 Create() => new NodeType_0041(); 
+    }
+    public class NodeType_0042 
+    { 
+        private NodeType_0042() {}
+        static public NodeType_0042 Create() => new NodeType_0042(); 
+    }
+    public class NodeType_0043 
+    { 
+        private NodeType_0043() {}
+        static public NodeType_0043 Create() => new NodeType_0043(); 
+    }
+    public class NodeType_0044 
+    { 
+        private NodeType_0044() {}
+        static public NodeType_0044 Create() => new NodeType_0044(); 
+    }
+    public class NodeType_0045 
+    { 
+        private NodeType_0045() {}
+        static public NodeType_0045 Create() => new NodeType_0045(); 
+    }
+    public class NodeType_0046 
+    { 
+        private NodeType_0046() {}
+        static public NodeType_0046 Create() => new NodeType_0046(); 
+    }
+    public class NodeType_0047 
+    { 
+        private NodeType_0047() {}
+        static public NodeType_0047 Create() => new NodeType_0047(); 
+    }
+    public class NodeType_0048 
+    { 
+        private NodeType_0048() {}
+        static public NodeType_0048 Create() => new NodeType_0048(); 
+    }
+    public class NodeType_0049 
+    { 
+        private NodeType_0049() {}
+        static public NodeType_0049 Create() => new NodeType_0049(); 
+    }
+    public class NodeType_0050 
+    { 
+        private NodeType_0050() {}
+        static public NodeType_0050 Create() => new NodeType_0050(); 
+    }
+    public class NodeType_0051 
+    { 
+        private NodeType_0051() {}
+        static public NodeType_0051 Create() => new NodeType_0051(); 
+    }
+    public class NodeType_0052 
+    { 
+        private NodeType_0052() {}
+        static public NodeType_0052 Create() => new NodeType_0052(); 
+    }
+    public class NodeType_0053 
+    { 
+        private NodeType_0053() {}
+        static public NodeType_0053 Create() => new NodeType_0053(); 
+    }
+    public class NodeType_0054 
+    { 
+        private NodeType_0054() {}
+        static public NodeType_0054 Create() => new NodeType_0054(); 
+    }
+    public class NodeType_0055 
+    { 
+        private NodeType_0055() {}
+        static public NodeType_0055 Create() => new NodeType_0055(); 
+    }
+    public class NodeType_0056 
+    { 
+        private NodeType_0056() {}
+        static public NodeType_0056 Create() => new NodeType_0056(); 
+    }
+    public class NodeType_0057 
+    { 
+        private NodeType_0057() {}
+        static public NodeType_0057 Create() => new NodeType_0057(); 
+    }
+    public class NodeType_0058 
+    { 
+        private NodeType_0058() {}
+        static public NodeType_0058 Create() => new NodeType_0058(); 
+    }
+    public class NodeType_0059 
+    { 
+        private NodeType_0059() {}
+        static public NodeType_0059 Create() => new NodeType_0059(); 
+    }
+    public class NodeType_0060 
+    { 
+        private NodeType_0060() {}
+        static public NodeType_0060 Create() => new NodeType_0060(); 
+    }
+    public class NodeType_0061 
+    { 
+        private NodeType_0061() {}
+        static public NodeType_0061 Create() => new NodeType_0061(); 
+    }
+    public class NodeType_0062 
+    { 
+        private NodeType_0062() {}
+        static public NodeType_0062 Create() => new NodeType_0062(); 
+    }
+    public class NodeType_0063 
+    { 
+        private NodeType_0063() {}
+        static public NodeType_0063 Create() => new NodeType_0063(); 
+    }
+    public class NodeType_0064 
+    { 
+        private NodeType_0064() {}
+        static public NodeType_0064 Create() => new NodeType_0064(); 
+    }
+    public class NodeType_0065 
+    { 
+        private NodeType_0065() {}
+        static public NodeType_0065 Create() => new NodeType_0065(); 
+    }
+    public class NodeType_0066 
+    { 
+        private NodeType_0066() {}
+        static public NodeType_0066 Create() => new NodeType_0066(); 
+    }
+    public class NodeType_0067 
+    { 
+        private NodeType_0067() {}
+        static public NodeType_0067 Create() => new NodeType_0067(); 
+    }
+    public class NodeType_0068 
+    { 
+        private NodeType_0068() {}
+        static public NodeType_0068 Create() => new NodeType_0068(); 
+    }
+    public class NodeType_0069 
+    { 
+        private NodeType_0069() {}
+        static public NodeType_0069 Create() => new NodeType_0069(); 
+    }
+    public class NodeType_0070 
+    { 
+        private NodeType_0070() {}
+        static public NodeType_0070 Create() => new NodeType_0070(); 
+    }
+    public class NodeType_0071 
+    { 
+        private NodeType_0071() {}
+        static public NodeType_0071 Create() => new NodeType_0071(); 
+    }
+    public class NodeType_0072 
+    { 
+        private NodeType_0072() {}
+        static public NodeType_0072 Create() => new NodeType_0072(); 
+    }
+    public class NodeType_0073 
+    { 
+        private NodeType_0073() {}
+        static public NodeType_0073 Create() => new NodeType_0073(); 
+    }
+    public class NodeType_0074 
+    { 
+        private NodeType_0074() {}
+        static public NodeType_0074 Create() => new NodeType_0074(); 
+    }
+    public class NodeType_0075 
+    { 
+        private NodeType_0075() {}
+        static public NodeType_0075 Create() => new NodeType_0075(); 
+    }
+    public class NodeType_0076 
+    { 
+        private NodeType_0076() {}
+        static public NodeType_0076 Create() => new NodeType_0076(); 
+    }
+    public class NodeType_0077 
+    { 
+        private NodeType_0077() {}
+        static public NodeType_0077 Create() => new NodeType_0077(); 
+    }
+    public class NodeType_0078 
+    { 
+        private NodeType_0078() {}
+        static public NodeType_0078 Create() => new NodeType_0078(); 
+    }
+    public class NodeType_0079 
+    { 
+        private NodeType_0079() {}
+        static public NodeType_0079 Create() => new NodeType_0079(); 
+    }
+    public class NodeType_0080 
+    { 
+        private NodeType_0080() {}
+        static public NodeType_0080 Create() => new NodeType_0080(); 
+    }
+    public class NodeType_0081 
+    { 
+        private NodeType_0081() {}
+        static public NodeType_0081 Create() => new NodeType_0081(); 
+    }
+    public class NodeType_0082 
+    { 
+        private NodeType_0082() {}
+        static public NodeType_0082 Create() => new NodeType_0082(); 
+    }
+    public class NodeType_0083 
+    { 
+        private NodeType_0083() {}
+        static public NodeType_0083 Create() => new NodeType_0083(); 
+    }
+    public class NodeType_0084 
+    { 
+        private NodeType_0084() {}
+        static public NodeType_0084 Create() => new NodeType_0084(); 
+    }
+    public class NodeType_0085 
+    { 
+        private NodeType_0085() {}
+        static public NodeType_0085 Create() => new NodeType_0085(); 
+    }
+    public class NodeType_0086 
+    { 
+        private NodeType_0086() {}
+        static public NodeType_0086 Create() => new NodeType_0086(); 
+    }
+    public class NodeType_0087 
+    { 
+        private NodeType_0087() {}
+        static public NodeType_0087 Create() => new NodeType_0087(); 
+    }
+    public class NodeType_0088 
+    { 
+        private NodeType_0088() {}
+        static public NodeType_0088 Create() => new NodeType_0088(); 
+    }
+    public class NodeType_0089 
+    { 
+        private NodeType_0089() {}
+        static public NodeType_0089 Create() => new NodeType_0089(); 
+    }
+    public class NodeType_0090 
+    { 
+        private NodeType_0090() {}
+        static public NodeType_0090 Create() => new NodeType_0090(); 
+    }
+    public class NodeType_0091 
+    { 
+        private NodeType_0091() {}
+        static public NodeType_0091 Create() => new NodeType_0091(); 
+    }
+    public class NodeType_0092 
+    { 
+        private NodeType_0092() {}
+        static public NodeType_0092 Create() => new NodeType_0092(); 
+    }
+    public class NodeType_0093 
+    { 
+        private NodeType_0093() {}
+        static public NodeType_0093 Create() => new NodeType_0093(); 
+    }
+    public class NodeType_0094 
+    { 
+        private NodeType_0094() {}
+        static public NodeType_0094 Create() => new NodeType_0094(); 
+    }
+    public class NodeType_0095 
+    { 
+        private NodeType_0095() {}
+        static public NodeType_0095 Create() => new NodeType_0095(); 
+    }
+    public class NodeType_0096 
+    { 
+        private NodeType_0096() {}
+        static public NodeType_0096 Create() => new NodeType_0096(); 
+    }
+    public class NodeType_0097 
+    { 
+        private NodeType_0097() {}
+        static public NodeType_0097 Create() => new NodeType_0097(); 
+    }
+    public class NodeType_0098 
+    { 
+        private NodeType_0098() {}
+        static public NodeType_0098 Create() => new NodeType_0098(); 
+    }
+    public class NodeType_0099 
+    { 
+        private NodeType_0099() {}
+        static public NodeType_0099 Create() => new NodeType_0099(); 
+    }
+}
